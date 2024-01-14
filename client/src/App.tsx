a1dbef0d93bfc8f82b521d906e2a33a7dbd09663
Hassan Hossam <egoker1234@gmail.com>
2024-01-14 20:24:41 +0200

    updating

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,89 +5,22 @@ import { Home } from './pages/home/home'
 import { Login } from "./pages/auth/login";
 import "./styles/global.scss";
 import { Register } from "./pages/auth/register";
-import { toast, ToastContainer } from 'react-toastify';
+import {  ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useEffect, useMemo, useState } from "react";
 import { grades, url } from "./utils/variables";
 import { User } from "./utils/interfaces";
 import { AddLecture } from "./pages/addLecture/add";
 import { Edit } from "./pages/edit/edit";
-import { Loading } from "./utils/components";
 
 
-const PopUser = ({ isAuth, setUser }: { isAuth: boolean }) => {
-  const [popUp, setPopUp] = useState(false);
-  const [userTemp, setUserTemp] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [cookie, setCookie] = useState(document.cookie);
 
-  const setCookies = () => {
-    const now = new Date();
-    const expires = new Date(now.getTime() + 60 * 60 * 1000);
-    document.cookie = `popUp=none; expires=${expires.toUTCString()}; path=/`;
-    setCookie(document.cookie)
-  }
-
-  const cancel = () => {
-    setCookies()
-    setPopUp(false);
-  }
-
-  useEffect(() => {
-    if (isAuth || !popUp || !userTemp || loading) return;
-    setLoading(true);
-    fetch(`${url}/auth/userTemp`, {
-      method: 'GET',
-      credentials: 'include',
-      headers: { "Content-Type": "application/json" }
-    })
-      .then(async (res) => res.json())
-      .then((res) => {
-        setUserTemp(false);
-        setLoading(false);
-        if (!res.success) throw new Error(res.data.msg);
-        setUser(res.data);
-        setPopUp(false);
-        setCookies();
-      }).catch((err) => {
-        setUserTemp(false)
-        setLoading(false);
-        toast.error(err.message);
-      })
-  }, [isAuth, loading, popUp, setUser, userTemp]);
-
-
-  useEffect(() => {
-    const timeoutId = setTimeout(() => { !isAuth && !cookie && setPopUp(true); }, 1000);
-    return () => { clearTimeout(timeoutId) };
-  }, [cookie, isAuth]);
-
-  return <div className={`popup-user-temp ${popUp && "show"}`}>
-    <div className="container">
-      <div className="text">
-        <span>  Welcome to our world!</span>
-        <br />
-        If you'd like to experience this web as an admin, please confirm this message.
-      </div>
-      <div className="buttons">
-        {!isAuth && !cookie &&
-          <>
-            <button className="cancel" onClick={cancel}>cancel</button>
-            <button onClick={() => setUserTemp(true)}>
-              {!loading && "Confirm"}
-              {loading && <Loading type="white" />}
-            </button>
-          </>
-        }
-      </div>
-    </div>
-  </div >
-}
 
 function App() {
   const [user, setUser] = useState<User | null>(null);
   const isAuth = useMemo(() => !!user, [user]);
   const [grade, setGrade] = useState<string | null>(localStorage.getItem('grade'));
+  
   useEffect(() => {
     const index = grades.findIndex((g) => g === grade);
     if (index < 0 || !grade) return;
@@ -138,7 +71,6 @@ function App() {
   ]);
 
   return <div className="app">
-    <PopUser isAuth={isAuth} setUser={setUser} />
     <RouterProvider router={router} />
     <ToastContainer />
   </div>
--- client/src/pages/home/home.tsx
+++ client/src/pages/home/home.tsx
@@ -7,7 +7,7 @@ import { url } from "../../utils/variables";
 import { toast } from "react-toastify";
 import { Tables, User } from "../../utils/interfaces";
 import { Table } from "../../components/table/table";
-import { Title } from "../../utils/components";
+import { Loading, Title } from "../../utils/components";
 
 interface Home {
   user: User | null;
@@ -15,6 +15,77 @@ interface Home {
   grade: string | null;
   setGrade: React.Dispatch<React.SetStateAction<string | null>>;
   setUser: React.Dispatch<React.SetStateAction<User | null>>;
+}
+
+
+
+const PopUser = ({ isAuth, setUser }: { isAuth: boolean }) => {
+  const [popUp, setPopUp] = useState(false);
+  const [userTemp, setUserTemp] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [cookie, setCookie] = useState(document.cookie);
+
+  const setCookies = () => {
+    const now = new Date();
+    const expires = new Date(now.getTime() + 60 * 60 * 1000);
+    document.cookie = `popUp=none; expires=${expires.toUTCString()}; path=/`;
+    setCookie(document.cookie)
+  }
+
+  const cancel = () => {
+    setCookies()
+    setPopUp(false);
+  }
+
+  useEffect(() => {
+    if (isAuth || !popUp || !userTemp || loading) return;
+    setLoading(true);
+    fetch(`${url}/auth/userTemp`, {
+      method: 'GET',
+      credentials: 'include',
+      headers: { "Content-Type": "application/json" }
+    })
+      .then(async (res) => res.json())
+      .then((res) => {
+        setUserTemp(false);
+        setLoading(false);
+        if (!res.success) throw new Error(res.data.msg);
+        setUser(res.data);
+        setPopUp(false);
+        setCookies();
+      }).catch((err) => {
+        setUserTemp(false)
+        setLoading(false);
+        toast.error(err.message);
+      })
+  }, [isAuth, loading, popUp, setUser, userTemp]);
+
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => { !isAuth && !cookie && setPopUp(true); }, 1000);
+    return () => { clearTimeout(timeoutId) };
+  }, [cookie, isAuth]);
+
+  return <div className={`popup-user-temp ${popUp && "show"}`}>
+    <div className="container">
+      <div className="text">
+        <span>  Welcome to our world!</span>
+        <br />
+        If you'd like to experience this web as an admin, please confirm this message.
+      </div>
+      <div className="buttons">
+        {!isAuth && !cookie &&
+          <>
+            <button className="cancel" onClick={cancel}>cancel</button>
+            <button onClick={() => setUserTemp(true)}>
+              {!loading && "Confirm"}
+              {loading && <Loading type="white" />}
+            </button>
+          </>
+        }
+      </div>
+    </div>
+  </div >
 }
 
 export const Home = ({ user, setUser, isAuth, grade, setGrade }: Home) => {
@@ -43,6 +114,7 @@ export const Home = ({ user, setUser, isAuth, grade, setGrade }: Home) => {
   return <>
     <Title title='Home' />
     <Nav grade={grade} setGrade={setGrade} user={user} setUser={setUser} />
+    <PopUser isAuth={isAuth} setUser={setUser} />
     <div className="contain">
       <div className="content-container">
         <div className="home">
